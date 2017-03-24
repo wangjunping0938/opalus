2bbf6fb40776df635c5ef01c6109c9fc294f559e
tianshuai <tianshuaiorc@sina.com>
2017-03-24 10:05:46 +0800

    readme

--- README.md
+++ README.md
@@ -6,7 +6,12 @@
 
 
 ### 环境布署
+- 进入当前项目目录
+- 创建虚拟环境：/opt/python3/bin/virtualenv env
+- 切换到虚拟环境：source /opt/project/python/test/venv/bin/activate
+- 关闭虚拟环境：deactivate
 
+- 通过pip install -r requirements.txt在该环境下进行安装。
 
 
 ### 启动程序uwsgi:
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_script import Manager, Server
 
 app = create_app(os.getenv('FLASK_ENV') or 'development')
 manager = Manager(app)
-manager.add_command("runserver", Server(host="127.0.0.1", port=5000, use_debugger=True))
+manager.add_command("runserver", Server(host="127.0.0.1", port=8002, use_debugger=True))
 
 if __name__ == '__main__':
     manager.run()
