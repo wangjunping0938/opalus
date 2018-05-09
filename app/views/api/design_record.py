2c594313466712c65c4fcba2c506eb627db69aa2
tianshuai <tianshuaiorc@sina.com>
2018-05-09 22:43:09 +0800

    add design record view

---
+++ app/views/api/design_record.py
@@ -0,0 +1,87 @@
+from flask import request, jsonify, current_app
+from . import api
+import datetime
+from app.models.design_record import DesignRecord
+from app.models.design_company import DesignCompany
+from app.helpers.pager import Pager
+from app.helpers.common import force_int
+
+## 列表
+@api.route('/design_record/list')
+def design_record_list():
+
+    query = {}
+    meta = {}
+    page = int(request.args.get('page', 1))
+    per_page = int(request.args.get('per_page', 10))
+    status = int(request.args.get('status', 1))
+    deleted = force_int(request.args.get('deleted', 0))
+    sort = force_int(request.args.get('sort', 1))
+    mark = request.args.get('mark', '')
+    no = int(request.args.get('no', 0))
+    if status == -1:
+        query['status'] = 0
+    if status == 1:
+        query['status'] = 1
+    else:
+        pass
+
+    if not mark or not no:
+        return jsonify(code=3001, message='缺少请求参数！')
+
+    query['mark'] = mark
+    query['no'] = no
+    query['deleted'] = deleted
+
+    # 排序
+    sortVal = '-ave_score'
+    if sort:
+        if sort == 1:
+            sortVal = '-ave_score'
+        elif sort == 2:
+            sortVal = '-base_average'
+        elif sort == 3:
+            sortVal = '-business_average'
+        elif sort == 4:
+            sortVal = '-innovate_average'
+        elif sort == 5:
+            sortVal = '-design_average'
+        elif sort == 6:
+            sortVal = '-effect_average'
+        elif sort == 7:
+            sortVal = '-credit_average'
+
+    try:
+        fields = ['_id', 'mark', 'no', 'ave_score', 'base_average', 'business_average', 'credit_average', 'design_average', 'effect_average', 'innovate_average', 'total_ave_score', 'type', 'number', 'deleted', 'status']
+        data = DesignRecord.objects(**query).order_by(sortVal).paginate(page=page, per_page=per_page)
+        total_count = DesignRecord.objects(**query).count()
+        current_app.logger.debug('aaaa')
+
+        # 过滤数据
+        rows = []
+        for i, d in enumerate(data.items):
+            row = {}
+            for f in fields:
+                if f in d:
+                    row[f] = d[f]
+            row['_id'] = str(d._id)
+            row['design_company'] = {}
+            #row.test = 'abc'
+            designCompany = DesignCompany.objects(number=int(d.number)).fields(['_id', 'name', 'logo_url', 'd3ing_id']).first()
+            if designCompany:
+                row['design_company'] = designCompany
+            rows.append(row)
+
+        meta['rows'] = rows
+    except(Exception) as e:
+        current_app.logger.debug(str(e))
+        meta['rows'] = []
+        total_count = 0
+
+    meta['total_count'] = total_count
+    meta['page'] = page
+    meta['per_page'] = per_page
+
+    return jsonify(code=0, message='success!', data=meta)
+
+
