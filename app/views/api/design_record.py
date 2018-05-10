5eb19c8815e5cdb728bab8761f97f3566d895efa
tianshuai <tianshuaiorc@sina.com>
2018-05-10 09:55:29 +0800

    record

--- app/views/api/design_record.py
+++ app/views/api/design_record.py
@@ -5,6 +5,7 @@ from app.models.design_record import DesignRecord
 from app.models.design_company import DesignCompany
 from app.helpers.pager import Pager
 from app.helpers.common import force_int, filter_key
+from bson import ObjectId
 
 ## 列表
 @api.route('/design_record/list')
@@ -12,47 +13,53 @@ def design_record_list():
 
     query = {}
     meta = {}
+    ids = request.args.get('ids', '')
     page = int(request.args.get('page', 1))
     per_page = int(request.args.get('per_page', 10))
     status = int(request.args.get('status', 1))
     deleted = force_int(request.args.get('deleted', 0))
-    sort = force_int(request.args.get('sort', 1))
+    sort = force_int(request.args.get('sort', 0))
     mark = request.args.get('mark', '')
     no = int(request.args.get('no', 0))
-    if status == -1:
-        query['status'] = 0
-    if status == 1:
-        query['status'] = 1
+
+    if ids:
+      query['_id__in'] = list(map(lambda x: ObjectId(x), ids.split(',')))
     else:
-        pass
+        if status == -1:
+            query['status'] = 0
+        if status == 1:
+            query['status'] = 1
+        else:
+            pass
 
-    if not mark or not no:
-        return jsonify(code=3001, message='缺少请求参数！')
+        if not mark or not no:
+            return jsonify(code=3001, message='缺少请求参数！')
 
-    query['mark'] = mark
-    query['no'] = no
+        query['mark'] = mark
+        query['no'] = no
+
     query['deleted'] = deleted
+
+    #current_app.logger.debug(query)
 
     # 排序
     sortVal = '-ave_score'
     if sort:
         if sort == 1:
-            sortVal = '-ave_score'
-        elif sort == 2:
             sortVal = '-base_average'
-        elif sort == 3:
+        elif sort == 2:
             sortVal = '-business_average'
-        elif sort == 4:
+        elif sort == 3:
             sortVal = '-innovate_average'
-        elif sort == 5:
+        elif sort == 4:
             sortVal = '-design_average'
-        elif sort == 6:
+        elif sort == 5:
             sortVal = '-effect_average'
-        elif sort == 7:
+        elif sort == 6:
             sortVal = '-credit_average'
 
     try:
-        fields = ['_id', 'mark', 'no', 'ave_score', 'base_average', 'business_average', 'credit_average', 'design_average', 'effect_average', 'innovate_average', 'total_ave_score', 'type', 'number', 'deleted', 'status']
+        fields = ['_id', 'mark', 'no', 'ave_score', 'base_average', 'business_average', 'credit_average', 'design_average', 'effect_average', 'innovate_average', 'ave_score', 'type', 'number', 'deleted', 'status']
         cFields = ['_id', 'name', 'logo_url', 'd3ing_id', 'description', 'scope_business']
         data = DesignRecord.objects(**query).order_by(sortVal).paginate(page=page, per_page=per_page)
         total_count = DesignRecord.objects(**query).count()
