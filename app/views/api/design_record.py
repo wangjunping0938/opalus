159317921817443e3c8b8f20d5ec9702a5088d8a
tianshuai <tianshuaiorc@sina.com>
2018-05-14 11:55:40 +0800

    record

--- app/views/api/design_record.py
+++ app/views/api/design_record.py
@@ -67,7 +67,7 @@ def design_record_list():
             sortVal = '-credit_average'
 
     try:
-        fields = ['_id', 'mark', 'no', 'ave_score', 'base_average', 'business_average', 'credit_average', 'design_average', 'effect_average', 'innovate_average', 'ave_score', 'type', 'number', 'deleted', 'status']
+        fields = ['_id', 'mark', 'no', 'ave_score', 'base_average', 'business_average', 'credit_average', 'design_average', 'effect_average', 'innovate_average', 'ave_score', 'type', 'number', 'rank', 'deleted', 'status']
         cFields = ['_id', 'name', 'logo_url', 'd3ing_id', 'description', 'scope_business']
         data = DesignRecord.objects(**query).order_by(sortVal).paginate(page=page, per_page=per_page)
         total_count = DesignRecord.objects(**query).count()
