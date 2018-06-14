0c6976024bfe8a2349ea52183efb2187a0870e74
wangjunping <592045723@qq.com>
2018-06-14 16:21:09 +0800

    删除了spider目录

--- spider/sSpider/bin/startspider.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-script_path=$(cd "$(dirname "$0")";cd ../;pwd)
-time_now=$(date "+%Y-%m-%d-%T")
-log_name=$1
-echo $time_now >> $script_path/log/"$log_name".log
-nohup scrapy crawl xspider >> $script_path/log/"$log_name".log 2>&1 &
-echo -e "\n\n" >> $script_path/log/"$log_name".log
