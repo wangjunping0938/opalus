c45277bc28a5a481d30a5af25302491cd33e67e5
LIMr1209 <aaa1058169464@126.com>
2018-11-19 16:49:55 +0800

    download

--- app/env.py
+++ app/env.py
@@ -3,4 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 cf = configparser.ConfigParser()
-cf.read(os.path.abspath(os.path.join(basedir, '..', '.env')))
+cf.read(os.path.abspath(os.path.join(basedir, '..', '.env_example')))
---
+++ app/jobs/Download.py
@@ -0,0 +1,41 @@
+import time
+from app.models.image import Image
+import os
+import requests
+from app.helpers.common import gen_mongo_id
+from flask import current_app
+from app import create_app
+
+app = create_app()
+def save_image(response, image):
+    if image:
+        local_name = image.name
+        bucket_name = current_app.config['QN_BUCKET_NAME']
+        local_path = "%s/%s/%s/%s" % (bucket_name, 'image', time.strftime("%y%m%d"), gen_mongo_id())
+        if not os._exists(local_path):
+            os.makedirs(local_path)
+        with open(local_path + '/' + local_name, 'wb') as f:
+            f.write(response.content)
+        image.local_path = local_path
+        image.local_name = image.name
+        image.save()
+
+
+def download():
+    image_all = Image.objects.all()
+    if image_all:
+        for image in image_all:
+            image_id = image._id
+            if image.local_path:
+                image = Image.objects(_id=image_id).first()
+                if image:
+                    try:
+                        response = requests.get(image.img_url)
+                        save_image(response, image)
+                    except Exception as e:
+                        print('下载图片失败', str(image_id))
+    else:
+        print('无图片')
+
+
+download()
