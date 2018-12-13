1c5dc252af48a31d34b581e96ac4d2d6e6d657fa
tianshuai <tianshuaiorc@sina.com>
2018-12-13 15:13:58 +0800

    image

--- app/views/main/image.py
+++ app/views/main/image.py
@@ -78,8 +78,6 @@ def image_list():
             meta['css_plane'] = 'active'
         meta['css_all'] = ''
         query['kind'] = kind
-    else:
-        query['kind'] = 1
 
     if status == -1:
         meta['css_disable'] = 'active'
