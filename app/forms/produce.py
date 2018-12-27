d81c71dd527e1f7335bbeca57952339fda2fe191
LIMr1209 <aaa1058169464@126.com>
2018-12-27 15:34:02 +0800

    produce save

--- app/forms/produce.py
+++ app/forms/produce.py
@@ -107,32 +107,38 @@ class SaveApi(FlaskForm):
     id = StringField()
     title = StringField('标题', validators=[Length(max=100, message="长度小于100个字符")])
     kind = IntegerField('类型')
-    channel = StringField('渠道', validators=[Length(max=30, message="长度小于30个字符")])  # 渠道
-    url = StringField('原文地址', validators=[Length(max=500, message="长度小于500字符")])  # 原文地址
     tags = StringField('标签', validators=[Length(max=500, message="长度小于500个字符")])  # 标签
     color_tags = StringField('颜色标签', validators=[Length(max=500, message="长度小于500个字符")])  # 颜色标签
     brand_tags = StringField('品牌标签', validators=[Length(max=500, message="长度小于500个字符")])  # 品牌标签
     material_tags = StringField('材质标签', validators=[Length(max=500, message="长度小于500个字符")])  # 材质
     style_tags = StringField('风格标签', validators=[Length(max=500, message="长度小于500个字符")])  # 风格
-    technique_tags = StringField('风格标签', validators=[Length(max=500, message="长度小于500个字符")])  # 风格
+    technique_tags = StringField('工艺标签', validators=[Length(max=500, message="长度小于500个字符")])  # 工艺
     other_tags = StringField('其它标签', validators=[Length(max=500, message="长度小于500个字符")])  # 其它
-    remark = StringField()
+    total_tags = StringField('所有标签')  # 所有标签
+    url = StringField('原文地址', validators=[Length(max=500, message="长度小于500字符")])  # 原文地址
     price = StringField()  # 销售价
     currency_type = IntegerField()  # 币种: 1.RMB；2.美元；3.--；
-    brand_id = IntegerField()  # 品牌ID
-    prize_id = IntegerField()  # 奖项ID
+    remark = StringField()
     designer = StringField()
     company = StringField()
-    prize = StringField()
-    prize_level = StringField()
-    prize_time = StringField()
+    domain = IntegerField()
     info = StringField()
+    channel = StringField('渠道', validators=[Length(max=30, message="长度小于30个字符")])  # 渠道
+    brand_id = IntegerField()  # 品牌ID
     evt = IntegerField()  # 来源
+    category_id = IntegerField()  # 分类ID
+    user_id = IntegerField()
+    asset_type = IntegerField()
+    asset_ids = StringField()
+    cover_id = StringField()
+    editor_id = IntegerField()
+    is_editor = IntegerField()
+    editor_level = IntegerField()
+    prize = FieldList(FormField(PrizeForm))
+    img_urls = FieldList(StringField())
 
-    def validate_url(self, field):
-        if field.data:
-            if Produce.objects(url=field.data).first():
-                raise ValueError('产品已存在!')
+
+
 
     def save(self, **param):
         data = self.data
--- app/views/api/produce.py
+++ app/views/api/produce.py
@@ -1,35 +1,46 @@
 from flask import request, jsonify, current_app
 from . import api
-import datetime
 from app.models.produce import Produce
-from app.helpers.pager import Pager
 from bson import ObjectId
 from app.forms.produce import SaveApi
-
+from app.models.image import Image
 
 
 ## 保存/更新
 @api.route('/produce/submit', methods=['POST'])
 def produce_submit():
-
+    ok = False
     form = SaveApi(request.values)
     if not form.validate_on_submit():
         return jsonify(code=3004, message=str(form.errors))
-
     data = request.values.to_dict()
-
     if not data:
         return jsonify(code=3003, message='至少传入一个参数!')
-
     try:
-        item = Produce(**data)
-        ok = item.save()
+        produce = Produce.objects(title=data['title'], channel=data['channel']).first()
+        if not produce:
+            produce_data = data.copy()
+            produce_data.pop('img_urls')
+            produce = Produce(**produce_data)
+            produce.save()
+        img_urls = data['img_urls'].split(',')
+        for i in img_urls:
+            img_data = {}
+            img_data['title'] = data['title']
+            img_data['img_url'] = i
+            img_data['channel'] = data['channel']
+            img_data['url'] = data['url']
+            img_data['target_id'] = str(produce._id)
+            img = Image.objects(img_url=i).first()
+            if not img:
+                img = Image(**img_data)
+                img.save()
+            ok = produce.update(cover_id=str(img._id))
 
         if ok:
-            return jsonify(code=0, message='success!', data=item)
+            return jsonify(code=0, message='success!', data=produce)
         else:
             return jsonify(code=3010, message='操作失败！')
 
     except(Exception) as e:
         return jsonify(code=3011, message=str(e))
-
